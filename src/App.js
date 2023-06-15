ab6dc36f72134f51a8738a3bddd05e20586ba3ee
Ajabs9 <jacobhonour9@gmail.com>
2023-06-15 03:27:01 +0100

    install reactjs and tailwind css and create the first template

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl bg-red-400">Hello World!</h1>
+    </>
+  );
+}
+
+export default App;
